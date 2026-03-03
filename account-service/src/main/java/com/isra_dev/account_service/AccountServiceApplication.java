2952156ec029381ca3c8ad5bcc2bf7b8978b2f71
IsraelVelaDev <digitalrobert000@gmail.com>
2026-03-03 14:44:13 -0400

    feat: Creacion y configuracion de los modulos transaction y account

---
+++ account-service/src/main/java/com/isra_dev/account_service/AccountServiceApplication.java
@@ -0,0 +1,12 @@
+package com.isra_dev.account_service;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AccountServiceApplication.class, args);
+    }
+}
---
+++ transaction-service/src/main/java/com/isra_dev/transaction_service/config/DataSourceConfig.java
@@ -0,0 +1,4 @@
+package com.isra_dev.transaction_service.config;
+
+public class DataSourceConfig {
+}
